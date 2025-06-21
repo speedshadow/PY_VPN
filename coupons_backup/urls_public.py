a657dfd68eec9ac18159b8420af532e7122492c2
speedshadow <alextg.96@gmail.com>
2025-06-21 14:56:38 +0100

    Remove coupons_backup folder (not used by project)

--- coupons_backup/urls_public.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views_public import coupon_public_list
-
-urlpatterns = [
-    path('', coupon_public_list, name='coupon_public_list'),
-]
--- coupons_backup/views_public.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Coupon
-
-def coupon_public_list(request):
-    coupons = Coupon.objects.filter(is_active=True)
-    return render(request, 'coupons/coupon_list_public.html', {'coupons': coupons})
